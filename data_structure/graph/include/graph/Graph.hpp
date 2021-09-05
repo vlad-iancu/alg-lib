cc550c41749fe065bb0b89225b20d1b0a1b2aa39
Iancu Vlad <iancuvladalexandru@gmail.com>
2021-09-05 11:50:37 +0300

    Graph: Added Graph interface

---
+++ data_structure/graph/include/graph/Graph.hpp
@@ -0,0 +1,24 @@
+#ifndef ALG_LIB_DATA_STRUCTURE_GRAPH_GRAPH_H_
+#define ALG_LIB_DATA_STRUCTURE_GRAPH_GRAPH_H_
+
+#include <vector>
+
+namespace graph
+{
+    typedef unsigned long long SizeG;
+
+    class Graph
+    {
+    protected:
+        SizeG node_count;
+        Graph() = delete;
+        Graph(const Graph &other) = delete;
+        Graph &operator=(const Graph &other) = delete;
+    public:
+        Graph(int node_count);
+        SizeG get_node_count();
+        virtual std::vector<int> get_neighbors(int u) = 0;
+    };
+} //namespace graph
+
+#endif
